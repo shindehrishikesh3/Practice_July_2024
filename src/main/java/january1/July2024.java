b632ac6e167780755f94603888097b39ef67858d
shindehrishikesh3 <shindehrishikesh3@gmail.com>
2024-07-20 15:23:03 +0530

    newFileJuly2024

---
+++ src/main/java/january1/July2024.java
@@ -0,0 +1,24 @@
+package january1;
+
+import java.util.Scanner;
+
+public class July2024 
+{
+
+public static void main(String[] args) 
+{
+
+	System.out.println("Calculator: ");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("First Value: ");
+	int a=sc.nextInt();
+	System.out.println("Second Value: ");
+	int b=sc.nextInt();
+	
+	int c=a+b;
+	System.out.println("Addition of " + a + " + " + b + " is : " + c );
+	
+		
+	
+}	
+}
